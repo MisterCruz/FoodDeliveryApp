cfa75ab43efa11fa1d7f7e88aa02296c5f44c2c6
MisterCruz <ID+username@users.noreply.github.com>
2022-10-31 21:05:22 -0700

    add paid status icon

--- pages/orders/[id].jsx
+++ pages/orders/[id].jsx
@@ -4,7 +4,38 @@ import Image from "next/image"
 const Order = () => {
   return (
     <div className={styles.container}> 
-        <div className={styles.left}></div>
+        <div className={styles.left}>
+            <div className={styles.row}><table className={styles.table}>
+                    <tbody>
+                    <tr className={styles.tr}>
+                    <th>Order ID</th>
+                    <th>Customer</th>
+                    <th>Address</th>
+                    <th>Total</th>
+                </tr>
+                <tr>
+                    <td>
+                        <span className={styles.id}>1289129</span>
+                    </td>
+                    <td>
+                        <span className={styles.name}>John Doe</span>
+                    </td>
+                    <td>
+                        <span className={styles.address}>Elton St. 212-33 SF</span>
+                    </td>
+                    <td>
+                        <span className={styles.total}>$39.80</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.status}>
+                    <Image src="/img/paid.png" width={30} height={30} alt="" />
+                </div>
+            </div>
+        </div>
         <div className={styles.right}>
         <div className={styles.wrapper}>
                 <h2 className={styles.title}>CART TOTAL</h2>
