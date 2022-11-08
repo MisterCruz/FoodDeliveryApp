d8833bd74e09e8979d7406906cf053b1e47b357b
MisterCruz <ID+username@users.noreply.github.com>
2022-11-08 10:54:49 -0800

    remove documentation comment

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -10,7 +10,6 @@ import {
 
 
 const Cart = () => {
-    // This values are the props in the UI
     const amount = "2";
     const currency = "USD";
     const style = {"layout":"vertical"};
@@ -18,10 +17,8 @@ const Cart = () => {
     const dispatch = useDispatch();
     const cart = useSelector(state=> state.cart);
     
-    // Custom component to wrap the PayPalButtons and handle currency changes
+    
 const ButtonWrapper = ({ currency, showSpinner }) => {
-    // usePayPalScriptReducer can be use only inside children of PayPalScriptProviders
-    // This is the main reason to wrap the PayPalButtons in a new component
     const [{ options, isPending }, dispatch] = usePayPalScriptReducer();
 
     useEffect(() => {
@@ -55,13 +52,13 @@ const ButtonWrapper = ({ currency, showSpinner }) => {
                             ],
                         })
                         .then((orderId) => {
-                            // Your code here after create the order
+                            // 
                             return orderId;
                         });
                 }}
                 onApprove={function (data, actions) {
                     return actions.order.capture().then(function () {
-                        // Your code here after capture the order
+                        // 
                     });
                 }}
             />
